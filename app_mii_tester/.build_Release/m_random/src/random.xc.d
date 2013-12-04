d89b23ed90267679b9259f32909c27bae1eb9d18
yuvaraj velumani <yuvaraj@xmos.com>
2013-12-04 17:16:39 +0530

    Delete random.xc.d

--- app_mii_tester/.build_Release/m_random/src/random.xc.d
+++
@@ -1,4 +0,0 @@
-.build_Release/m_random/src/random.xc.d ./.build_Release/m_random/src/random.xc.o: \
-  ../../epl_mii_tester_script_intrf/module_random/src/random.xc \
-  ../../epl_mii_tester_script_intrf/module_random/src/random.h
-
