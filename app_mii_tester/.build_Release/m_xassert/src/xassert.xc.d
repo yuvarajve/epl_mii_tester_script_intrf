140e00649f673be2526d8e6474dc7e83e4cf3043
yuvaraj velumani <yuvaraj@xmos.com>
2013-12-04 17:17:04 +0530

    Delete xassert.xc.d

--- app_mii_tester/.build_Release/m_xassert/src/xassert.xc.d
+++
@@ -1,5 +0,0 @@
-.build_Release/m_xassert/src/xassert.xc.d ./.build_Release/m_xassert/src/xassert.xc.o: \
-  ../../epl_mii_tester_script_intrf/module_xassert/src/xassert.xc \
-  ../../epl_mii_tester_script_intrf/module_xassert/src/xassert.h \
-  ../../epl_mii_tester_script_intrf/app_mii_tester/src/debug_conf.h
-
