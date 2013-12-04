3069a8fb92a29410d50fbdf4c6b48cd2954ef564
yuvaraj velumani <yuvaraj@xmos.com>
2013-12-04 17:16:01 +0530

    Delete swlock.c.d

--- app_mii_tester/.build_Release/m_locks/src/swlock.c.d
+++
@@ -1,3 +0,0 @@
-.build_Release/m_locks/src/swlock.c.d ./.build_Release/m_locks/src/swlock.c.o:  \
- ../../epl_mii_tester_script_intrf/module_locks/src/swlock.c \
-  ../../epl_mii_tester_script_intrf/module_locks/src/swlock.h
