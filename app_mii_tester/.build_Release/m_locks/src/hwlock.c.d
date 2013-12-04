df019f07ed89ea32143acbac6d8f1ce943d3beb2
yuvaraj velumani <yuvaraj@xmos.com>
2013-12-04 17:15:50 +0530

    Delete hwlock.c.d

--- app_mii_tester/.build_Release/m_locks/src/hwlock.c.d
+++
@@ -1,3 +0,0 @@
-.build_Release/m_locks/src/hwlock.c.d ./.build_Release/m_locks/src/hwlock.c.o:  \
- ../../epl_mii_tester_script_intrf/module_locks/src/hwlock.c \
-  ../../epl_mii_tester_script_intrf/module_locks/src/hwlock.h
