da7c213c7ce0232586c40478a1478e82e1eed352
yuvaraj velumani <yuvaraj@xmos.com>
2013-12-04 17:16:50 +0530

    Delete random_init.c.d

--- app_mii_tester/.build_Release/m_random/src/random_init.c.d
+++
@@ -1,3 +0,0 @@
-.build_Release/m_random/src/random_init.c.d ./.build_Release/m_random/src/random_init.c.o:  \
- ../../epl_mii_tester_script_intrf/module_random/src/random_init.c \
-  ../../epl_mii_tester_script_intrf/module_random/src/random.h
